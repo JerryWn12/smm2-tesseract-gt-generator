2f4ea716006a88bdf1c3264eb112647f087b593a
JerryWn <2571290541qq@gmail.com>
2023-04-26 11:09:09 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+from PIL import Image, ImageDraw, ImageFont
+from random import random
+
+MODEL_NAME = "Smm2-hori"
+FONT_FILENAME = "smm2.ttf"
+
+
+def generate(name, text):
+
+    font = ImageFont.truetype(f"fonts/{FONT_FILENAME}", size=64)
+    length = font.getlength(text)
+
+    image = Image.new("RGB", (int(length), 48), "white")
+
+    draw = ImageDraw.Draw(image, "RGB")
+    draw.text((0, -10), text, (0, 0, 0), font)
+
+    image.save(f"gt/{MODEL_NAME}-{name}.png")
+
+    try:
+        with open(f"gt/{MODEL_NAME}-{name}.gt.txt", mode="x") as file:
+            file.write(text)
+    except OSError as err:
+        print(f"error: {str(err)}, skip")
+
+
+def generate_course_id(count):
+
+    # smm2 course id do not contain I,O,Z
+    chars = "1234567890ABCDEFGHJKLMNPQRSTUVWXY"
+    length = len(chars)
+
+    for _ in range(count):
+        id = ""
+        for _ in range(9):
+            index = int(random() * length)
+            char = chars[index]
+            id = id + char
+            if len(id) in (3, 7):
+                id = id + "-"
+        generate(f"{id[:3]}{id[-3:]}", id)
+
+
+def generate_score(count):
+
+    for _ in range(count):
+        # smm2 have a maxinum score of 8000
+        score = str(int(random() * 8000))
+        generate(score, score)
+
+
+generate_course_id(10)
+generate_score(20)
